de268f73836a26d2f18dac71f2bac5a7018d8bf1
Machu13 <machu130@gmail.com>
2019-01-11 16:21:39 +0100

    Update config.h

--- config.h
+++ config.h
@@ -3,14 +3,40 @@
 
 #include "type.h"
 
+/**
+ * @brief InitConfig
+ * @param Param
+ */
 void InitConfig (CConfig & Param);
 
+/**
+ * @brief LoadConfig
+ * @param Param
+ */
 void LoadConfig (CConfig & Param);
 
-int readInt32(const std::string & Val);
+/**
+ * @brief readInt32
+ * @param Param
+ * @param Val
+ * @return
+ */
+const int readInt32(CConfig & Param, const std::string & Val);
 
-char readChar(const std::string & Val);
+/**
+ * @brief readChar
+ * @param Param
+ * @param Val
+ * @return
+ */
+const char readChar(CConfig & Param, const std::string & Val);
 
-std::string readString(const std::string & Val);
+/**
+ * @brief readString
+ * @param Param
+ * @param Val
+ * @return
+ */
+const std::string readString(CConfig & Param, const std::string & Val);
 
 #endif // CONFIG_H
