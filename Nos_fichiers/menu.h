df58de1251c09c5dfd2f774b5ae2b17c876ec3ad
Machu13 <machu130@gmail.com>
2019-01-14 16:30:53 +0100

    Update menu.h

--- Nos_fichiers/menu.h
+++ Nos_fichiers/menu.h
@@ -5,10 +5,11 @@ class Menu
 {
 private:
     static char GetChoice ();
-    static void ConfigMenu ();
+    static void MovePointer (char & choice, unsigned & nbr, const unsigned & max);
+    static void ConfigMenu (unsigned nbr);
     static void ShowConfig ();
     static void CreditsMenu ();
-    static void MainMenu ();
+    static void MainMenu (unsigned nbr);
 public:
     static void ShowConfigMenu ();
     static void ShowCreditsMenu ();
